638ac3454e9196a2662d2046d6bddd2726e865b9
Danik <Papsuev331@gmail.com>
2025-03-11 16:18:41 +0300

    009

--- гос/main/code.js
+++ гос/main/code.js
@@ -16,54 +16,5 @@ var swiper = new Swiper('.swiper-container', {
 
 
 
-// Находим все изображения и модальные окна
-const images = document.querySelectorAll('.category_img');
-const modals = document.querySelectorAll('.modal');
-const closeButtons = document.querySelectorAll('.close');
-
-// Открытие модального окна
-images.forEach(img => {
-  img.addEventListener('click', () => {
-    const modalId = img.dataset.modal; 
-    const modal = document.getElementById(modalId);
-    if (modal) {
-      modal.style.display = 'flex';
-    }
-  });
-});
-
-// Закрытие модального окна
-closeButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modalId = button.dataset.modal; 
-    const modal = document.getElementById(modalId);
-    if (modal) {
-      modal.style.display = 'none';
-    }
-  });
-});
-
-// Закрытие при клике на фон
-modals.forEach(modal => {
-  modal.addEventListener('click', event => {
-    if (event.target === modal) {
-      modal.style.display = 'none';
-    }
-  });
-});
-
-
-function showModal(icon) {
-  // Делаем иконку белой
-  icon.classList.toggle('active');
-
-  // Показать или скрыть модальное окно
-  var modal = document.getElementById('modal');
-  if (modal.style.display === 'flex') {
-      modal.style.display = 'none';
-  } else {
-      modal.style.display = 'flex';
-  }
-}
 
 
